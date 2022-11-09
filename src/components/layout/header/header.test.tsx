6a65588135fa1f92a2d6459293969badf70ad4b2
chmery <karektv@gmail.com>
2022-11-09 19:26:48 +0100

    Create unit tests for header

---
+++ src/components/layout/header/header.test.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from "@testing-library/react";
+import Header from "./header";
+
+describe("Header", () => {
+    it("should render sign in link when user is not authorized", () => {
+        render(<Header isAuthorized={false} />);
+        const signInLink = screen.getByText("Sign In");
+        expect(signInLink).toBeVisible();
+    });
+
+    it("should render new listing link if user is authorized", () => {
+        render(<Header isAuthorized />);
+        const newListing = screen.getByText("New Listing");
+        expect(newListing).toBeVisible();
+    });
+});
---
+++ src/components/layout/header/header.tsx
@@ -0,0 +1,11 @@
+import styles from "header.module.scss";
+
+type Props = {
+    isAuthorized: boolean;
+};
+
+const Header = ({ isAuthorized }: Props) => {
+    return <h1>Header</h1>;
+};
+
+export default Header;
