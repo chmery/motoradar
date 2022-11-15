1173334f9cdc5dac4b8a51452fccd8e652a2b4ef
chmery <karektv@gmail.com>
2022-11-15 09:01:16 +0100

    General filter styles improvement

--- src/components/Filter/CustomCheckbox/CustomCheckbox.tsx
+++ src/components/Filter/CustomCheckbox/CustomCheckbox.tsx
@@ -1,6 +1,7 @@
 import { Checkbox } from '@mui/material'
 import { ChangeEvent } from 'react'
 import { ImCheckboxChecked, ImCheckboxUnchecked } from 'react-icons/im'
+import styles from './CustomCheckbox.module.scss'
 
 type Props = {
   label: string
@@ -13,25 +14,30 @@ const CustomCheckbox = ({ label, onChange }: Props) => {
     isChecked: boolean
   ) => onChange(isChecked)
 
+  const capitalizedLabel = `${label[0].toUpperCase()}${label.slice(1)}`
+
   return (
-    <Checkbox
-      disableRipple
-      checkedIcon={<ImCheckboxChecked />}
-      icon={<ImCheckboxUnchecked />}
-      sx={{
-        color: 'var(--placeholder)',
-        svg: { fontSize: '1.5rem' },
-        '&.MuiButtonBase-root': {
-          background: 'transparent',
-          padding: '0',
-        },
-        '&.Mui-checked': { color: 'var(--blue)', background: '#fff' },
-      }}
-      inputProps={{
-        'aria-label': `${label}`,
-      }}
-      onChange={onChangeHandler}
-    />
+    <div className={styles.checkbox}>
+      <Checkbox
+        disableRipple
+        checkedIcon={<ImCheckboxChecked />}
+        icon={<ImCheckboxUnchecked />}
+        sx={{
+          color: 'var(--placeholder)',
+          svg: { fontSize: '1.5rem' },
+          '&.MuiButtonBase-root': {
+            background: 'transparent',
+            padding: '0',
+          },
+          '&.Mui-checked': { color: 'var(--blue)', background: '#fff' },
+        }}
+        inputProps={{
+          'aria-label': `${label}`,
+        }}
+        onChange={onChangeHandler}
+      />
+      <span className={styles.text}>{capitalizedLabel}</span>
+    </div>
   )
 }
 
--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -23,6 +23,7 @@ const RANGE_DATA = {
 
 const Filter = () => {
   const [isDamaged, setIsDamaged] = useState(false)
+  const [isAccidentFree, setIsAccidentFree] = useState(false)
   const [brand, setBrand] = useState('')
   const [chassis, setChassis] = useState('')
   const [yearRange, setYearRange] = useState(RANGE_DATA.range)
@@ -31,12 +32,15 @@ const Filter = () => {
   const chassisSelectHandler = (selected: string) => setChassis(selected)
   const yearRangeSelectHandler = (range: number[]) => setYearRange(range)
   const damagedCheckHandler = (isChecked: boolean) => setIsDamaged(isChecked)
+  const accidentFreeCheckHandler = (isChecked: boolean) =>
+    setIsAccidentFree(isChecked)
 
   const searchHandler = (event: React.FormEvent) => {
     event.preventDefault()
 
     const searchParams = {
       isDamaged,
+      isAccidentFree,
       brand,
       chassis,
       yearRange,
@@ -48,15 +52,23 @@ const Filter = () => {
   return (
     <form className={styles.filter} onSubmit={searchHandler}>
       <h3>What you're looking for?</h3>
-      <DropdownList
-        title={TEST_DATA.title}
-        options={TEST_DATA.options}
-        onSelect={brandSelectHandler}
-      />
-      <DropdownList
-        title={TEST_DATA2.title}
-        options={TEST_DATA2.options}
-        onSelect={chassisSelectHandler}
+      <div className={styles.dropdowns}>
+        <DropdownList
+          title={TEST_DATA.title}
+          options={TEST_DATA.options}
+          onSelect={brandSelectHandler}
+        />
+        <DropdownList
+          title={TEST_DATA2.title}
+          options={TEST_DATA2.options}
+          onSelect={chassisSelectHandler}
+        />
+      </div>
+      <RangeSlider
+        title={RANGE_DATA.title}
+        defaultValue={RANGE_DATA.defaultValue}
+        range={RANGE_DATA.range}
+        onChange={yearRangeSelectHandler}
       />
       <RangeSlider
         title={RANGE_DATA.title}
@@ -64,7 +76,19 @@ const Filter = () => {
         range={RANGE_DATA.range}
         onChange={yearRangeSelectHandler}
       />
-      <CustomCheckbox label={'damaged'} onChange={damagedCheckHandler} />
+      <RangeSlider
+        title={RANGE_DATA.title}
+        defaultValue={RANGE_DATA.defaultValue}
+        range={RANGE_DATA.range}
+        onChange={yearRangeSelectHandler}
+      />
+      <div className={styles.checkboxes}>
+        <CustomCheckbox label={'damaged'} onChange={damagedCheckHandler} />
+        <CustomCheckbox
+          label={'accident-free'}
+          onChange={accidentFreeCheckHandler}
+        />
+      </div>
       <Button text='Search' type='submit' />
     </form>
   )
