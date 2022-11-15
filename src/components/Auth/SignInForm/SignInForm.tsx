8f42b58ad188dd115df474704ead792f63d7ef4c
Paweł Kromołowski <cater981pl@gmail.com>
2022-11-15 18:47:22 +0100

    sign in page

---
+++ src/components/Auth/SignInForm/SignInForm.tsx
@@ -0,0 +1,95 @@
+import RadialLoader from 'components/Loaders/RadialLoader/RadialLoader';
+import { FirebaseError } from 'firebase/app';
+import { useRouter } from 'next/router';
+import { FormEvent, useState } from 'react';
+import { AuthType, useAuth } from 'store/AuthContext';
+import { getAuthErrorMessage } from 'utils/getAuthErrorMessage';
+import styles from './SignInForm.module.scss';
+
+const SignInForm = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const { signIn } = useAuth() as AuthType;
+  const router = useRouter();
+
+  const handleInputChange = (type: string, input: string) => {
+    setError('');
+
+    switch (type) {
+      case 'email':
+        setEmail(input);
+        break;
+      case 'password':
+        setPassword(input);
+        break;
+      default:
+        console.error(
+          'passed wrong type to handleInputChange function in sign-up'
+        );
+        break;
+    }
+  };
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    setLoading(true);
+
+    if (!email && !password) return;
+
+    try {
+      await signIn(email, password);
+      router.push('/');
+    } catch (error) {
+      if (error instanceof FirebaseError) {
+        setError(getAuthErrorMessage(error.code));
+      }
+    }
+    setLoading(false);
+  };
+
+  return (
+    <form className={styles.form} onSubmit={handleSubmit} noValidate>
+      <label
+        htmlFor='email'
+        className={`${styles.label} ${error ? styles.labelError : ''}`}
+      >
+        E-mail
+      </label>
+      <input
+        type='email'
+        id='email'
+        autoComplete='test@test.com'
+        value={email}
+        onChange={(e) => handleInputChange('email', e.target.value)}
+        className={`${styles.input} ${error ? styles.inputError : ''}`}
+      />
+      <label
+        htmlFor='password'
+        className={`${styles.label} ${error ? styles.labelError : ''}`}
+      >
+        Password
+      </label>
+      <input
+        type='password'
+        id='password'
+        autoComplete='new-password'
+        value={password}
+        onChange={(e) => handleInputChange('password', e.target.value)}
+        className={`${styles.input} ${error ? styles.inputError : ''}`}
+      />
+      {error && <span className={styles.error}>{error}</span>}
+      <button
+        type='submit'
+        className={`${styles.button} ${email && password ? styles.active : ''}`}
+        disabled={email && password && !loading ? false : true}
+      >
+        {loading ? <RadialLoader /> : 'Sign In'}
+      </button>
+    </form>
+  );
+};
+
+export default SignInForm;
--- src/pages/sign-in/index.tsx
+++ src/pages/sign-in/index.tsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import SignInForm from 'components/Auth/SignInForm/SignInForm';
+import Link from 'next/link';
+import styles from './index.module.scss';
 
 const SignInPage = () => {
-  return <div>index</div>;
+  return (
+    <div className={styles.main}>
+      <h1 className={styles.header}>Sign In</h1>
+      <SignInForm />
+      <p>
+        Don't have an account?{' '}
+        <Link href='/sign-up' className={styles.link}>
+          Sign Up
+        </Link>
+      </p>
+    </div>
+  );
 };
 
 export default SignInPage;
