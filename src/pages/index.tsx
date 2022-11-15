19c12ed33a7f0ebca50c9574707af9f48b687b40
chmery <karektv@gmail.com>
2022-11-15 10:17:31 +0100

    Create landing page styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,17 @@
 import Filter from 'components/Filter/Filter'
+import Wrapper from 'components/UI/Wrapper/Wrapper'
+import styles from './index.module.scss'
 
 export default function Home() {
-  return <Filter />
+  return (
+    <div className={styles.background}>
+      <div className={styles.gradient}>
+        <Wrapper>
+          <div className={styles.filter}>
+            <Filter />
+          </div>
+        </Wrapper>
+      </div>
+    </div>
+  )
 }
