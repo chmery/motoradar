ee7a786c25f4cc10c1e2bb1924eff311ee548168
Paweł Kromołowski <cater981pl@gmail.com>
2022-11-16 19:55:20 +0100

    better error prompt for user

--- src/components/Auth/SignInForm/SignInForm.tsx
+++ src/components/Auth/SignInForm/SignInForm.tsx
@@ -5,19 +5,19 @@ import { AuthType, useAuth } from 'store/AuthContext';
 import { getAuthErrorMessage } from 'utils/getAuthErrorMessage';
 import styles from './SignInForm.module.scss';
 import RadialLoader from 'components/UI/Loaders/RadialLoader/RadialLoader';
+import ErrorBox from '../ErrorBox/ErrorBox';
 
 const SignInForm = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
+  const [isErrorOpen, setIsErrorOpen] = useState(false);
   const [loading, setLoading] = useState(false);
 
   const { signIn } = useAuth() as AuthType;
   const router = useRouter();
 
   const handleInputChange = (type: string, input: string) => {
-    setError('');
-
     switch (type) {
       case 'email':
         setEmail(input);
@@ -45,50 +45,55 @@ const SignInForm = () => {
     } catch (error) {
       if (error instanceof FirebaseError) {
         setError(getAuthErrorMessage(error.code));
+        setIsErrorOpen(true);
       }
     }
     setLoading(false);
   };
 
+  const handleErrorBoxClose = () => {
+    setIsErrorOpen(false);
+  };
+
   return (
-    <form className={styles.form} onSubmit={handleSubmit} noValidate>
-      <label
-        htmlFor='email'
-        className={`${styles.label} ${error ? styles.labelError : ''}`}
-      >
-        E-mail
-      </label>
-      <input
-        type='email'
-        id='email'
-        autoComplete='test@test.com'
-        value={email}
-        onChange={(e) => handleInputChange('email', e.target.value)}
-        className={`${styles.input} ${error ? styles.inputError : ''}`}
-      />
-      <label
-        htmlFor='password'
-        className={`${styles.label} ${error ? styles.labelError : ''}`}
-      >
-        Password
-      </label>
-      <input
-        type='password'
-        id='password'
-        autoComplete='new-password'
-        value={password}
-        onChange={(e) => handleInputChange('password', e.target.value)}
-        className={`${styles.input} ${error ? styles.inputError : ''}`}
-      />
-      {error && <span className={styles.error}>{error}</span>}
-      <button
-        type='submit'
-        className={`${styles.button} ${email && password ? styles.active : ''}`}
-        disabled={email && password && !loading ? false : true}
-      >
-        {loading ? <RadialLoader /> : 'Sign In'}
-      </button>
-    </form>
+    <>
+      {isErrorOpen && (
+        <ErrorBox text={error} closeErrorBox={handleErrorBoxClose} />
+      )}
+      <form className={styles.form} onSubmit={handleSubmit} noValidate>
+        <label htmlFor='email' className={styles.label}>
+          E-mail
+        </label>
+        <input
+          type='email'
+          id='email'
+          autoComplete='test@test.com'
+          value={email}
+          onChange={(e) => handleInputChange('email', e.target.value)}
+          className={styles.input}
+        />
+        <label htmlFor='password' className={styles.label}>
+          Password
+        </label>
+        <input
+          type='password'
+          id='password'
+          autoComplete='new-password'
+          value={password}
+          onChange={(e) => handleInputChange('password', e.target.value)}
+          className={styles.input}
+        />
+        <button
+          type='submit'
+          className={`${styles.button} ${
+            email && password ? styles.active : ''
+          }`}
+          disabled={email && password && !loading ? false : true}
+        >
+          {loading ? <RadialLoader /> : 'Sign In'}
+        </button>
+      </form>
+    </>
   );
 };
 
--- src/components/Auth/SignUpForm/SignUpForm.tsx
+++ src/components/Auth/SignUpForm/SignUpForm.tsx
@@ -7,20 +7,20 @@ import { getAuthErrorMessage } from 'utils/getAuthErrorMessage';
 
 import styles from './SignUpForm.module.scss';
 import RadialLoader from 'components/UI/Loaders/RadialLoader/RadialLoader';
+import ErrorBox from '../ErrorBox/ErrorBox';
 
 const SignUpForm = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
   const [error, setError] = useState('');
+  const [isErrorOpen, setIsErrorOpen] = useState(false);
   const [loading, setLoading] = useState(false);
 
   const { signUp } = useAuth() as AuthType;
   const router = useRouter();
 
   const handleInputChange = (type: string, input: string) => {
-    setError('');
-
     switch (type) {
       case 'email':
         setEmail(input);
@@ -47,6 +47,8 @@ const SignUpForm = () => {
 
     if (password !== confirmPassword) {
       setError('Passwords do not match!');
+      setIsErrorOpen(true);
+      setLoading(false);
       return;
     }
 
@@ -56,68 +58,68 @@ const SignUpForm = () => {
     } catch (error) {
       if (error instanceof FirebaseError) {
         setError(getAuthErrorMessage(error.code));
+        setIsErrorOpen(true);
       }
     }
     setLoading(false);
   };
 
+  const handleErrorBoxClose = () => {
+    setIsErrorOpen(false);
+  };
+
   return (
-    <form className={styles.form} onSubmit={handleSubmit} noValidate>
-      <label
-        htmlFor='email'
-        className={`${styles.label} ${error ? styles.labelError : ''}`}
-      >
-        E-mail
-      </label>
-      <input
-        type='email'
-        id='email'
-        autoComplete='test@test.com'
-        value={email}
-        onChange={(e) => handleInputChange('email', e.target.value)}
-        className={`${styles.input} ${error ? styles.inputError : ''}`}
-      />
-      <label
-        htmlFor='password'
-        className={`${styles.label} ${error ? styles.labelError : ''}`}
-      >
-        Password
-      </label>
-      <input
-        type='password'
-        id='password'
-        autoComplete='new-password'
-        value={password}
-        onChange={(e) => handleInputChange('password', e.target.value)}
-        className={`${styles.input} ${error ? styles.inputError : ''}`}
-      />
-      <label
-        htmlFor='confirm'
-        className={`${styles.label} ${error ? styles.labelError : ''}`}
-      >
-        Confirm password
-      </label>
-      <input
-        type='password'
-        id='confirm'
-        autoComplete='new-password'
-        value={confirmPassword}
-        onChange={(e) => handleInputChange('confirm', e.target.value)}
-        className={`${styles.input} ${error ? styles.inputError : ''}`}
-      />
-      {error && <span className={styles.error}>{error}</span>}
-      <button
-        type='submit'
-        className={`${styles.button} ${
-          email && password && confirmPassword ? styles.active : ''
-        }`}
-        disabled={
-          email && password && confirmPassword && !loading ? false : true
-        }
-      >
-        {loading ? <RadialLoader /> : 'Sign Up'}
-      </button>
-    </form>
+    <>
+      {isErrorOpen && (
+        <ErrorBox text={error} closeErrorBox={handleErrorBoxClose} />
+      )}
+      <form className={styles.form} onSubmit={handleSubmit} noValidate>
+        <label htmlFor='email' className={styles.label}>
+          E-mail
+        </label>
+        <input
+          type='email'
+          id='email'
+          autoComplete='test@test.com'
+          value={email}
+          onChange={(e) => handleInputChange('email', e.target.value)}
+          className={styles.input}
+        />
+        <label htmlFor='password' className={styles.label}>
+          Password
+        </label>
+        <input
+          type='password'
+          id='password'
+          autoComplete='new-password'
+          value={password}
+          onChange={(e) => handleInputChange('password', e.target.value)}
+          className={styles.input}
+        />
+        <label htmlFor='confirm' className={styles.label}>
+          Confirm password
+        </label>
+        <input
+          type='password'
+          id='confirm'
+          autoComplete='new-password'
+          value={confirmPassword}
+          onChange={(e) => handleInputChange('confirm', e.target.value)}
+          className={styles.input}
+        />
+        <button
+          type='submit'
+          className={`${styles.button} ${
+            email && password && confirmPassword ? styles.active : ''
+          }`}
+          disabled={
+            email && password && confirmPassword && !loading ? false : true
+          }
+        >
+          {loading ? <RadialLoader /> : 'Sign Up'}
+        </button>
+      </form>
+    </>
   );
 };
 
