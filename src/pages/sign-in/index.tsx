f717585ba980b486da676dfe9387626392551472
Paweł Kromołowski <cater981pl@gmail.com>
2022-11-16 19:00:35 +0100

    wrapped auth pages in Wrapper component

--- src/pages/sign-in/index.tsx
+++ src/pages/sign-in/index.tsx
@@ -1,19 +1,22 @@
 import SignInForm from 'components/Auth/SignInForm/SignInForm';
+import Wrapper from 'components/UI/Wrapper/Wrapper';
 import Link from 'next/link';
 import styles from './index.module.scss';
 
 const SignInPage = () => {
   return (
-    <div className={styles.main}>
-      <h1 className={styles.header}>Sign In</h1>
-      <SignInForm />
-      <p>
-        Don't have an account?{' '}
-        <Link href='/sign-up' className={styles.link}>
-          Sign Up
-        </Link>
-      </p>
-    </div>
+    <Wrapper>
+      <div className={styles.main}>
+        <h1 className={styles.header}>Sign In</h1>
+        <SignInForm />
+        <p>
+          Don't have an account?{' '}
+          <Link href='/sign-up' className={styles.link}>
+            Sign Up
+          </Link>
+        </p>
+      </div>
+    </Wrapper>
   );
 };
 
--- src/pages/sign-up/index.tsx
+++ src/pages/sign-up/index.tsx
@@ -3,23 +3,23 @@ import Link from 'next/link';
 
 import styles from './index.module.scss';
 import { AuthType, useAuth } from 'store/AuthContext';
-import { useRouter } from 'next/router';
-import { FirebaseError } from 'firebase/app';
-import { getAuthErrorMessage } from 'utils/getAuthErrorMessage';
 import SignUpForm from 'components/Auth/SignUpForm/SignUpForm';
+import Wrapper from 'components/UI/Wrapper/Wrapper';
 
 const SignUpPage = () => {
   return (
-    <div className={styles.main}>
-      <h1 className={styles.header}>Sign Up</h1>
-      <SignUpForm />
-      <p>
-        Already have an account?{' '}
-        <Link href='/sign-in' className={styles.link}>
-          Sign In
-        </Link>
-      </p>
-    </div>
+    <Wrapper>
+      <div className={styles.main}>
+        <h1 className={styles.header}>Sign Up</h1>
+        <SignUpForm />
+        <p>
+          Already have an account?{' '}
+          <Link href='/sign-in' className={styles.link}>
+            Sign In
+          </Link>
+        </p>
+      </div>
+    </Wrapper>
   );
 };
 
