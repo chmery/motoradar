c133e4554146c2265f8a0dbe4ba4e2ae4b34d68f
Paweł Kromołowski <cater981pl@gmail.com>
2022-12-06 17:53:52 +0100

    remaining information from listing

--- src/components/ListingPage/Page.tsx
+++ src/components/ListingPage/Page.tsx
@@ -25,6 +25,30 @@ const Page = ({ data, listingId }: Props) => {
       </section>
       <section className={styles.listing}>
         <Carousel images={data.imageUrls} listingId={listingId} />
+        <h4 className={styles.header}>Details</h4>
+        <div className={styles.details}>
+          <p className={styles['details__type']}>Brand</p>
+          <p>{data.brand}</p>
+          <p className={styles['details__type']}>Model</p>
+          <p>{data.model}</p>
+          <p className={styles['details__type']}>Production Year</p>
+          <p>{data.productionYear}</p>
+          <p className={styles['details__type']}>Mileage</p>
+          <p>{getNumberWithSpaces(data.mileage)}km</p>
+          <p className={styles['details__type']}>Power</p>
+          <p>{data.power}HP</p>
+          <p className={styles['details__type']}>Fuel Type</p>
+          <p>{data.fuelType}</p>
+          <p className={styles['details__type']}>Gearbox</p>
+          <p>{data.gearbox}</p>
+          <p className={styles['details__type']}>Powertrain</p>
+          <p>{data.powertrain}</p>
+        </div>
+        <h4 className={styles.header}>Description</h4>
+        <p className={styles.description}>{data.description}</p>
+        <p className={styles.date}>
+          Listed {new Date(data.date).toLocaleString()}
+        </p>
       </section>
     </article>
   );
