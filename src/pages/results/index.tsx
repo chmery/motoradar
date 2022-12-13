e0f86808d7498ecbb9eb2f07259cfec901b5f74e
Paweł Kromołowski <cater981pl@gmail.com>
2022-12-13 17:48:52 +0100

    sort dropdown

--- src/pages/results/index.tsx
+++ src/pages/results/index.tsx
@@ -2,14 +2,41 @@ import styles from './ResultsPage.module.scss';
 import { useRouter } from 'next/router';
 import Wrapper from '../../components/UI/Wrapper/Wrapper';
 import Filter from '../../components/Filter/Filter';
+import DropdownList from '../../components/UI/DropdownList/DropdownList';
+import { useState } from 'react';
+
+const SORT_OPTIONS = [
+  'Recently Added',
+  'Price Ascending',
+  'Price Descending',
+  'Mileage Ascending',
+  'Mileage Descending',
+  'Production Year Ascending',
+  'Production Year descending',
+];
 
 const ResultsPage = () => {
+  const [sortOption, setSortOption] = useState('date');
+
   const router = useRouter();
+
+  const handleSortOption = (selected: string) => {
+    setSortOption(selected);
+  };
 
   return (
     <Wrapper>
       <div className={styles.container}>
         <div className={styles.filters}>
+          <div className={styles['sort-container']}>
+            <h3 className={styles.header}>Sort</h3>
+            <DropdownList
+              placeholder={'Recently Added'}
+              options={SORT_OPTIONS}
+              onSelect={handleSortOption}
+              dark={router.pathname === '/'}
+            />
+          </div>
           <Filter />
         </div>
         <div className={styles.listings}>aha</div>
